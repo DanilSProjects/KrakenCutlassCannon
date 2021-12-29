09ec1355d6bd95e3b092240e7457468c7a31022d
DanilSProjects <danielsyrov9@gmail.com>
2021-12-29 16:10:23 +0800

    Computer turn function added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+let possiblePlays = ["Rock", "Paper", "Scissors"];
+
+// Functions for computer's turn + randomising it as much as possible
+function computerPlay() {
+    return possiblePlays[randomisePlay()];
+}
+
+function randomisePlay() {
+    let randomNo = Math.random();
+    let output = (randomNo >= 0.5) ? Math.floor(Math.random() * (possiblePlays.length - 1)) : Math.ceil(Math.random() * (possiblePlays.length - 1));
+    return output
+}
