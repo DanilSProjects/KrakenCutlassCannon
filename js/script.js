a01fb400a3e70b731ecab345ed1d10e24f15859f
DanilSProjects <danielsyrov9@gmail.com>
2021-12-29 16:58:25 +0800

    5 round game loop added

--- js/script.js
+++ js/script.js
@@ -19,20 +19,61 @@ function playRound(playerSelection, computerSelection) {
 
     // Error handling
     if (!possiblePlays.includes(playerSelection)) {
-        return "Error - you must input either rock, paper or scissors!";
+        return "ERROR";
     }
     
     // Cycle through each possible winning combination
     for (let i = 0; i < (combinations.length); i++) {
         if (playerSelection === combinations[i].win && computerSelection === combinations[i].lose) {
-            return "You Win!";
+            return "WIN";
         }
     }
 
     // If not winning, check for tie; otherwise lose
     if (playerSelection === computerSelection) {
-        return "Tie!";
+        return "TIE";
     } else {
-        return "You Lose!";
+        return "LOSE";
     }
+}
+
+// Main game
+function game() {
+    let maxRounds = 5;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < maxRounds;) {
+        let playerInput = prompt("Let's play for 5 rounds! Choose: rock, paper or scissors?");
+        let output = playRound(playerInput, computerPlay());
+
+        switch (output) {
+            case "WIN":
+                playerScore += 1;
+                i += 1;
+                alert(`You win!\n\nCurrent scores:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+                break;
+            case "TIE":
+                alert(`Tie!\n\nCurrent scores:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+                break;
+            case "LOSE":
+                computerScore += 1;
+                i += 1;
+                alert(`You lose!\n\nCurrent scores:\nPlayer: ${playerScore}\nComputer: ${computerScore}`);
+                break;
+            case "ERROR":
+                alert("Error - please input only rock, paper or scissors.");
+                break;
+            default:
+                alert("Something has gone terribly wrong. Try again!")
+                break;
+        }
+    }
+
+    finishGame(playerScore, computerScore);
+}
+
+function finishGame(playerScore, computerScore) {
+    alert(playerScore)
+    alert(computerScore)
 }
