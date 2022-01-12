4979764abe473597a883bf0c58330d1e9d6b5bce
DanilSProjects <danielsyrov9@gmail.com>
2022-01-12 19:05:19 +0800

    Added opening exposition + rules

--- js/script.js
+++ js/script.js
@@ -1,7 +1,66 @@
+// Variable setup
+let pirateExposition = [
+"Ahoy, landlubber! Had a good sleep? Har har!", 
+"The name's Bill, cap'n o' the Hallyho Men. Most of me lads call me Crazy Bill, no clue why.",
+"We're a whole family of friendly loot hunters and kindly murderers. Har har!",
+"Ye're like enough wonderin' how ye got here. Doesn't matter.",
+"What matters now is that your life hangs between the rum glass and the mouth, so to speak, laddie.",
+"Normally we kill any prisoners we get our grubby hands on, but I'm pretty bored - these numbskulls on the ship are hardly any good company - and you seem a sturdy enough lad.",
+"So, tell ye what - play a good game of Kraken, Cutlass, Cannon with me. If you win 3 rounds, you join the crew. If you lose, you get the privilege of walking the plank.",
+"The rules are simple:",
+"Kraken beats cannon - those slippery bastards rip cannons off the deck like meat off the bone.",
+"Cutlass beats kraken - you land a clean hit into the eye and that hellhound won't be coming back for decades.",
+'Cannon beats cutlass - as my pop used to say, "never bring a sabre to a cannon battle."',
+"Easy enough? Great, let's begin!"
+];
 let possiblePlays = ["KRAKEN", "CUTLASS", "CANNON"];
 let combinations = [{win:"KRAKEN", lose:"CANNON"}, {win:"CUTLASS", lose:"KRAKEN"}, {win:"CANNON", lose:"CUTLASS"}]
 let playerScore = 0;
 let computerScore = 0;
+
+// DOM Setup for pirate talk + scores paragraphs
+let pirateTalkPara = document.querySelector('#pirate_talk');
+let scoresPara = document.querySelector('#scores');
+
+// DOM Setup for buttons
+let choiceButtonsDiv = document.querySelector('#choice_buttons');
+let playerSelectButtons = document.querySelectorAll(".play");
+playerSelectButtons.forEach( (button) => {
+    button.addEventListener( 'click', (e) => {
+        game(button.getAttribute("id"));
+    })
+})
+
+// Function for the exposition and beginning of the game
+function beginExposition() {
+    playerSelectButtons.forEach((button) => {
+        button.style.display = "none";
+    })
+    let continueButton = document.createElement('button');
+    continueButton.textContent = "Continue";
+    continueButton.classList.add('play');
+
+    let expositionIndex= 0;
+    let expositionLimit = pirateExposition.length - 1;
+    pirateTalkPara.textContent = pirateExposition[expositionIndex];
+    continueButton.addEventListener('click', () => {
+        expositionIndex += 1;
+
+        if (expositionIndex <= expositionLimit) {
+            pirateTalkPara.textContent = pirateExposition[expositionIndex];
+        } else {
+            choiceButtonsDiv.removeChild(continueButton);
+            playerSelectButtons.forEach((button) => {
+                button.style.display = "flex";
+            })
+            pirateTalkPara.textContent = "I'm going to drink you like rum, lad!";
+        }
+    });
+
+    choiceButtonsDiv.appendChild(continueButton);
+}
+
+window.onload = beginExposition;
 
 // Functions for computer's turn + randomising it as much as possible
 function computerPlay() {
@@ -33,18 +92,6 @@ function playRound(playerSelection, computerSelection) {
         return "LOSE";
     }
 }
-
-// DOM Setup for buttons
-let playerSelectButtons = document.querySelectorAll("button");
-playerSelectButtons.forEach( (button) => {
-    button.addEventListener( 'click', (e) => {
-        game(button.getAttribute("id"));
-    })
-})
-
-// DOM Setup for pirate talk + scores paragraphs
-let pirateTalkPara = document.querySelector('#pirate_talk');
-let scoresPara = document.querySelector('#scores');
 
 // Main game
 function game(playerInput) {
@@ -84,7 +131,6 @@ function finishGame(playerScore, computerScore) {
     playerSelectButtons.forEach( (button) => {
         button.style.display = "none";
     })
-    let choiceButtonsDiv = document.querySelector('#choice_buttons');
     let replayButton = document.createElement('button');
     replayButton.textContent = "Play again?";
     replayButton.classList.add("replay");
